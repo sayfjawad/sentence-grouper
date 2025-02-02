49e44c2f6a810f0efda0c41bde4681a293605e40
sayf <sayf@multicode.nl>
2025-02-03 00:23:55 +0100

    Added more algorithms

--- src/test/java/nl/multicode/search/CormodeLZSimilaritySearchTest.java
+++ src/test/java/nl/multicode/search/CormodeLZSimilaritySearchTest.java
@@ -1,8 +1,10 @@
 package nl.multicode.search;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CormodeLZSimilaritySearchTest {
@@ -10,12 +12,12 @@ class CormodeLZSimilaritySearchTest {
     /**
      * Tests the functionality of the `CormodeLZSimilaritySearch` class to find sentences similar
      * to a given search sentence within a specified threshold using Cormode's LZ distance.
-     *
+     * <p>
      * The test verifies that the method correctly identifies sentences that are close to
      * "Albert Heijn" from a list of sentences and ensures the output meets the expected results.
      * Specifically, the test checks that:
      * - Sentences similar to "Albert Heijn", such as "Albört H ijn" and "Alber Heijn", are included
-     *   in the result based on the threshold.
+     * in the result based on the threshold.
      * - Irrelevant sentences ("Action", "Blokker", etc.) are excluded from the result.
      */
     @Test
--- src/test/java/nl/multicode/search/IndelSentenceSearchTest.java
+++ src/test/java/nl/multicode/search/IndelSentenceSearchTest.java
@@ -12,14 +12,14 @@ class IndelSentenceSearchTest {
     /**
      * Tests the functionality of the `SimilarSentenceSearch` class to find sentences similar
      * to a given search sentence within a specified threshold using Levenshtein distance.
-     *
+     * <p>
      * The test verifies that the method correctly identifies sentences that are close to
      * "Albert Heijn" from a list of sentences and ensures the output meets the expected results.
      * Specifically, the test checks that:
      * - Sentences similar to "Albert Heijn", such as "Albört H ijn" and "Alber Heijn", are included
-     *   in the result based on the threshold.
+     * in the result based on the threshold.
      * - Irrelevant sentences ("Action", "Blokker", etc.) are excluded from the result.
-     *
+     * <p>
      * This test uses assertions to validate the correctness of the result.
      */
     @Test
